e7956a3b1d4f6b789a83b1f4535859e764595122
Heath Williams <awilli01@amgen.com>
2017-09-28 16:27:50 -0400

    Fixed some issues with project

--- app/main.js
+++ app/main.js
@@ -5,56 +5,50 @@ var symbols = ['0', 'X'];
 var turn = 0;
 
 // Waits for page to load before doing interesting things
- document.onreadystatechange = function() {
- 	if (document.readyState === "interactive") {
- 		restartButton.onclick = startGame;
- 		startGame();
- 	}
- };
+document.onreadystatechange = function() {
+	if (document.readyState === "interactive") {
+		restartButton.onclick = startGame;
+		startGame();
+	}
+};
 
- function startGame() {
+function startGame() {
 
- 	// Reset turn counter
+	// Reset turn counter
+	turn = 0;
 
- 	turn = 0;
+	// Remove winner notification and make sure div is hidden
 
- 	// Remove winner notification and make sure div is hidden
+	// Clear board and add click events on squares
+	for (var i = 0; i < spaces.length; i++) {
+		spaces[i].addEventListener("click", takesSpace);
+	}
 
- 	// Clear board and add click events on squares
- 	for (var i = 0; i < spaces.length; i++) {
+}
 
- 		spaces[i].addEventListener("click", takesSpace);
- 		}
+function takesSpace() {
+	console.log(wins);
 
- 	}
+	turn++;
+	this.innerHTML = symbols[turn % 2];
+	this.removeEventListener("click", takesSpace);
 
- 	function takesSpace() {
- 		console.log(wins);
+	for ( var i=0; i < wins.length; i++) {
+		checkForWin(wins[i]);
+	}
 
- 		turn++;
- 		this.innerHTML = symbols[turn % 2];
- 		this.removeEventListener("click", takeSpace);
+}
 
- 		for ( var i=0; i < wins.length; i++) {
- 			checkForWin(wins[i]);
- 		}
+function checkForWin(winArray) {
 
- 	}
+	var temp = '';
 
- 	function checkForWin(winArray) {
+	for (var i = 0; i < winArray.length; i++) {
+		temp += spaces[i].innerHTML;
+	}
 
- 		var temp = '';
-
- 		for (var i = 0; i < winArray.length; i++) {
-
- 			temp += spaces[i].innerHTML;
- 		}
-
- 			console.log(temp);
- 			
- 
-
- 	}
+	console.log(temp);
+}
 
 
  
