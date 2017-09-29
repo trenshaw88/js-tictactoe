e2c15ded545123707b59283b1bfb93802ae696fe
trenshaw88 <trenshaw88@twc.com>
2017-09-29 00:18:03 -0400

    Updated main.js and styles,css

--- app/main.js
+++ app/main.js
@@ -3,52 +3,79 @@ var restartButton =  document.getElementById('restartButton');
 var spaces = document.getElementsByClassName('space');
 var symbols = ['0', 'X'];
 var turn = 0;
+var notification = document.getElementById('notification');
+var winnerMessage = document.getElementById('winnerMessage');
 
 // Waits for page to load before doing interesting things
-document.onreadystatechange = function() {
-	if (document.readyState === "interactive") {
-		restartButton.onclick = startGame;
-		startGame();
-	}
-};
+ document.onreadystatechange = function() {
+ 	if (document.readyState === "interactive") {
+ 		restartButton.onclick = startGame;
+ 		startGame();
+ 	}
+ };
 
-function startGame() {
+ function startGame() {
 
-	// Reset turn counter
-	turn = 0;
+ 	// Reset turn counter
 
-	// Remove winner notification and make sure div is hidden
+ 	turn = 0;
 
-	// Clear board and add click events on squares
-	for (var i = 0; i < spaces.length; i++) {
-		spaces[i].addEventListener("click", takesSpace);
-	}
+ 	// Remove winner notification and make sure div is hidden
+  winnerMessage.innerHTML = '';
+  notification.style.display = 'none'
+
+ 	// Clear board and add click events on squares
+ 	for (var i = 0; i < spaces.length; i++) {
+    spaces[i].innerHTML = '';
+ 		spaces[i].addEventListener("click", takesSpace);
+ 		}
+
+ 	}
+
+ 	function takesSpace() {
+
+ 		turn++;
+ 		this.innerHTML = symbols[turn % 2];
+ 		this.removeEventListener("click", takeSpace);
+
+ 		for (var i= 0; i < wins.length; i++) {
+ 		 if	(checkForWin(wins[i])) {
+		
+		// Clicking stops
+		for (var j = 0; j < spaces.length; j++) {
+        spaces[j].removeEventListener("click", takeSpace);
+      }
+
+       // Dislpay player victor notification
+       notification.style.display = 'block';
+       winnerMessage.innerHTML = "Yay! " + currentPlayer + " won!";
+
+ 		
+ 	}
+ 	else {
+ 		if (turn === 9) {
+
+       // Dislpay player draw notification
+ 			notification.style.display = "block";
+        	winnerMessage.innerHTML += "Bummer! It's a draw!";
+     	}
+      }
+ 	} 
+
+ 	}
+
+ function checkForWin(winArray) {
+
+  return spaces[winArray[0]].innerHTML !== '' && 
+    spaces[winArray[0]].innerHTML === spaces[winArray[1]].innerHTML && 
+    spaces[winArray[0]].innerHTML === spaces[winArray[2]].innerHTML;
 
 }
 
-function takesSpace() {
-	console.log(wins);
+ 			
+ 
 
-	turn++;
-	this.innerHTML = symbols[turn % 2];
-	this.removeEventListener("click", takesSpace);
 
-	for ( var i=0; i < wins.length; i++) {
-		checkForWin(wins[i]);
-	}
-
-}
-
-function checkForWin(winArray) {
-
-	var temp = '';
-
-	for (var i = 0; i < winArray.length; i++) {
-		temp += spaces[i].innerHTML;
-	}
-
-	console.log(temp);
-}
 
 
  
